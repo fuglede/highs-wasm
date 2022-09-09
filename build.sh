08ee1a5588f5fe70038288cadd9540a9ad32ed6a
Søren Fuglede Jørgensen <github@fuglede.dk>
2022-09-09 21:21:54 +0200

    Fix path to post.js in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd build
 emcmake cmake ../HiGHS -DOPENMP=OFF -DFAST_BUILD=OFF -DSHARED=OFF
 emmake make -j8 libhighs
-emcc -O3 -s EXPORTED_FUNCTIONS="['_Highs_mipCall']" -s EXPORT_NAME="Highs" --post-js post.js -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap','setValue','getValue']" -sMODULARIZE -s ALLOW_MEMORY_GROWTH=1 -flto lib/*.a -o highs.js
+emcc -O3 -s EXPORTED_FUNCTIONS="['_Highs_mipCall']" -s EXPORT_NAME="Highs" --post-js ../post.js -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap','setValue','getValue']" -sMODULARIZE -s ALLOW_MEMORY_GROWTH=1 -flto lib/*.a -o highs.js
